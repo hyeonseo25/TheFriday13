8e8d7a0da31c319813e39b4cc0ad07dbf196c1bb
Soomin <ssoomin1@naver.com>
2020-10-28 17:40:40 +0900

    Let's go commit

--- TheFriday_13/Assets/TextEffect.cs
+++ TheFriday_13/Assets/TextEffect.cs
@@ -8,6 +8,7 @@ public class TextEffect : MonoBehaviour
 {
     public Text Buy_cookie_txt;
     private string m_text = "그렇게 추자와 춘자는 쿠키를 사러 갔다.";
+    private string second_text = "얼마 후 ... ";
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,19 @@ public class TextEffect : MonoBehaviour
         {
             Buy_cookie_txt.text = m_text.Substring(0, i);
             //딜레이 주기
+            yield return new WaitForSeconds(0.15f);
+        }
+        StartCoroutine(_typing2());
+    }
+
+    IEnumerator _typing2()
+    {
+        yield return new WaitForSeconds(0.5f);
+        //타이핑 효과
+        for (int i = 0; i < second_text.Length; i++)
+        {
+            Buy_cookie_txt.text = second_text.Substring(0, i);
+            //딜레이주기
             yield return new WaitForSeconds(0.15f);
         }
     }
