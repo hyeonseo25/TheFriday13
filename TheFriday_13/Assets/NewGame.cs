47c78d14664576c66312c47270c81fdad659dfea
Soomin <ssoomin1@naver.com>
2020-11-05 23:01:46 +0900

    name_check 기본값설정

--- TheFriday_13/Assets/NewGame.cs
+++ TheFriday_13/Assets/NewGame.cs
@@ -10,11 +10,16 @@ using Debug = UnityEngine.Debug;
 public class NewGame : MonoBehaviour
 {
     public InputField input_nickname;
-    public static string ch_name = "춘자";
+    public static string ch_name;
 
     public void NewGameBtn()
     {
         Debug.Log("Hello World");
         ch_name = input_nickname.text;
+        Debug.Log(ch_name);
+        if (ch_name.Equals(""))
+        {
+            ch_name = "춘자";
+        }
     }
 }
